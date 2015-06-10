28dfbc0fd7fd6a96e3631be7df97778977b349e1
ArturoMC <arturo.mosqued@sizmek.com>
2015-06-10 11:34:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Chat.io
+
+Some chat app to do some stuff, it was built in meteor, tho'
+
---
+++ client/client.js
@@ -0,0 +1,31 @@
+Template.messages.helpers({
+    messages: function(){
+        return Messages.find({}, {sort:{time: -1}});
+    }
+});
+
+Template.input.events({
+    'keydown input#message' : function(event) {
+        if (event.which == 13) // obviously.. that is the enter key
+        {
+            var name = "Anon";
+
+            if (Meteor.user()){
+                name = Meteor.user().profile.name;
+            }
+
+            var message = document.getElementById('message');
+
+            if (message.value != ""){
+                Messages.insert({
+                    name: name,
+                    message: message.value,
+                    time: Date.now()
+                });
+
+                document.getElementById('message').value = "";
+                message.value = "";
+            }
+        }
+    }
+});
